e11041937bd9a82a45573d6229b300e3a51834d4
shadrack odielo <92942778+ShadrackOdielo@users.noreply.github.com>
2025-08-24 23:13:33 +0300

    update

--- app/components/AdminTable.vue
+++ app/components/AdminTable.vue
@@ -1,7 +1,128 @@
 <script setup lang="ts">
+import type { Transaction} from '~~/server/utils/drizzle'
+import type {  TableColumn,  } from '@nuxt/ui'
+import { upperFirst } from 'scule'
+import { getPaginationRowModel } from '@tanstack/vue-table'
 
-const transactions  = await useFetch('/api/mpesa/transactions.list' )
-    
+const UBadge = resolveComponent('UBadge')
+const UCheckbox = resolveComponent('UCheckbox')
+const table = useTemplateRef('table')
+// const data = ref<Transaction[]>([])
+const { data }  = await useFetch('/api/mpesa/transactions.list',
+   {
+    key: 'transations',
+    transform: (data?: Transaction[]) => {
+      return data
+    }
+  }
+ )
+
+const columns: TableColumn<Transaction>[] = [
+  {
+    id: 'select',
+    header: ({ table }) =>
+      h(UCheckbox, {
+        modelValue: table.getIsSomePageRowsSelected()
+          ? 'indeterminate'
+          : table.getIsAllPageRowsSelected(),
+        'onUpdate:modelValue': (value: boolean | 'indeterminate') =>
+          table.toggleAllPageRowsSelected(!!value),
+        'aria-label': 'Select all'
+      }),
+    cell: ({ row }) =>
+      h(UCheckbox, {
+        modelValue: row.getIsSelected(),
+        'onUpdate:modelValue': (value: boolean | 'indeterminate') => row.toggleSelected(!!value),
+        'aria-label': 'Select row'
+      })
+  },
+  {
+    accessorKey: 'id',
+    header: '#',
+    cell: ({ row }) => `#${row.getValue('id')}`
+  },
+  {
+    accessorKey: 'createdAt',
+    header: 'Date Created',
+    cell: ({ row }) => {
+      return new Date(row.getValue('createdAt')).toLocaleString('en-US', {
+        day: 'numeric',
+        month: 'short',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+      })
+    }
+  },
+    {
+    accessorKey: 'transactionDate',
+    header: 'Date',
+    cell: ({ row }) => {
+      return new Date(row.getValue('transactionDate')).toLocaleString('en-US', {
+        day: 'numeric',
+        month: 'short',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+      })
+    }
+  },
+    {
+    accessorKey: 'updatedAt',
+    header: 'Date Created',
+    cell: ({ row }) => {
+      return new Date(row.getValue('updatedAt')).toLocaleString('en-US', {
+        day: 'numeric',
+        month: 'short',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+      })
+    }
+  },
+  {
+    accessorKey: 'status',
+    header: 'Status',
+    cell: ({ row }) => {
+      const color = {
+        SUCCESS: 'success' as const,
+        FAILED: 'error' as const,
+        CANCELLED: 'error' as const,
+        PENDING: 'neutral' as const
+      }[row.getValue('status') as string]
+
+      return h(UBadge, { class: 'capitalize', variant: 'subtle', color }, () =>
+        row.getValue('status')
+      )
+    }
+  },
+  {
+    accessorKey: 'initiatorPhone',
+    header: 'Sender'
+  },
+  {
+    accessorKey: 'amount',
+    header: () => h('div', { class: 'text-right' }, 'Amount'),
+    cell: ({ row }) => {
+      const amount = Number.parseFloat(row.getValue('amount'))
+
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'KES'
+      }).format(amount)
+
+      return h('div', { class: 'text-right font-medium' }, formatted)
+    }
+  }
+]
+
+const columnVisibility = ref({
+  id: false
+})
+const pagination = ref({
+  pageIndex: 0,
+  pageSize: 5
+})
 </script>
 
 <template>
@@ -9,6 +130,51 @@ const transactions  = await useFetch('/api/mpesa/transactions.list' )
     <template #header>
         MPesa Transactions
     </template>
-  <UTable :data="transactions.data.value"  class="flex-1" />
+    <div class="flex justify-end px-4 py-3.5 border-b  border-accented">
+      <UDropdownMenu
+        :items="
+          table?.tableApi
+            ?.getAllColumns()
+            .filter((column) => column.getCanHide())
+            .map((column) => ({
+              label: upperFirst(column.id),
+              type: 'checkbox' as const,
+              checked: column.getIsVisible(),
+              onUpdateChecked(checked: boolean) {
+                table?.tableApi?.getColumn(column.id)?.toggleVisibility(!!checked)
+              },
+              onSelect(e?: Event) {
+                e?.preventDefault()
+              }
+            }))
+        "
+        :content="{ align: 'end' }"
+      >
+        <UButton
+          label="Columns"
+          color="neutral"
+          variant="outline"
+          trailing-icon="i-lucide-chevron-down"
+        />
+      </UDropdownMenu>
+    </div>
+  <UTable
+   ref="table"
+      v-model:pagination="pagination"
+      v-model:column-visibility="columnVisibility"
+      :data="data"
+      :columns="columns"
+      :pagination-options="{
+        getPaginationRowModel: getPaginationRowModel()
+      }"
+      class="flex-1"/>
+      <div class="flex justify-center border-t border-default pt-4">
+      <UPagination
+        :default-page="(table?.tableApi?.getState().pagination.pageIndex || 0) + 1"
+        :items-per-page="table?.tableApi?.getState().pagination.pageSize"
+        :total="table?.tableApi?.getFilteredRowModel().rows.length"
+        @update:page="(p) => table?.tableApi?.setPageIndex(p - 1)"
+      />
+    </div>
   </UCard>
 </template>
