e074813714b75739332dcad7d2f689a4299b5904
shadrack odielo <92942778+ShadrackOdielo@users.noreply.github.com>
2025-08-30 23:26:00 +0300

    errors

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 </script>
 <template>
-    <div class="w-screen max-w-screen  overflow-x-hidden relative flex flex-col">
-        <AppHeader class="fixed top-0" />
-        <div class="min-h-screen flex items-center align-center w-full" >
+    <div>
+        <AppHeader  />
         <slot />
-        </div>
-        <AppFooter class="" />
-    </div>
+        <AppFooter  />
+</div>
 </template>
