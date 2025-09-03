3bce9eb375447af844c253de78c7f1dc92714745
shadrack odielo <92942778+ShadrackOdielo@users.noreply.github.com>
2025-09-03 12:57:32 +0300

    admin auth

--- app/app.vue
+++ app/app.vue
@@ -5,3 +5,17 @@
     </NuxtLayout>
   </UApp>
 </template>
+<script setup lang="ts">
+const supabase = useSupabaseClient()
+const toast = useToast()
+supabase.auth.onAuthStateChange((event, session) => {
+  // Custom logic, e.g.:
+  if (event === 'SIGNED_IN') navigateTo('/dashboard')
+  console.log(session)
+  toast.add({
+    title: 'Login successfull',
+    description: 'You can now access your Dashboard'
+}
+)
+})
+</script>
