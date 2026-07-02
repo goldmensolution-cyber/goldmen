a486b1f5c368594da8c2a83ecf6190f6d8907dd0
shadrack odielo <92942778+ShadrackOdielo@users.noreply.github.com>
2026-07-02 18:55:21 +0300

    changes

--- app/middleware/authenticated.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtRouteMiddleware(() => {
-  const  user  = useSupabaseUser()
-
-  // redirect the user to the login screen if they're not authenticated
-  if (!user) {
-    return navigateTo('/login')
-  }
-})
-
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: true,
-  nitro: {
-    prerender: {
-      crawlLinks: true,
-      routes: ['/blog', '/help']
-    }
-  },
+ 
 
   modules: [
     '@nuxt/ui',
